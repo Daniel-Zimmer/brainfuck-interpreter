8f351584478cac7add41a3d1b8b0ad4276ae3443
Daniel Zimmer <daniel_zimmer@outlook.com>
2018-05-24 18:51:04 -0300

    pronto

--- brainfuck.lua
+++ brainfuck.lua
@@ -2,6 +2,12 @@ local code = ""
 local filename = "code.txt"
 local t = {0,0,0,0,0,0,0,0,0,0,0}
 local tpos = 1
+local loopTemp = {0,0,0,0,0,0,0,0,0,0,0}
+local loopTempPos = {0,0,0,0,0,0,0,0,0,0,0}
+local loopDepth = 0
+local stepDisplaySize = 6
+local stepDisplay = true
+local i = 1
 
 function fileload(filename)
   local file = io.open(filename)
@@ -16,7 +22,7 @@ end
 
 code = fileload(filename)
 
-for i = 1, #code, 1 do
+while (i <= #code) do
     if  string.sub(code,i,i) == "+" then
       t[tpos] = t[tpos] + 1
     elseif string.sub(code,i,i) == "-" and t[tpos] > 0 then
@@ -26,10 +32,39 @@ for i = 1, #code, 1 do
     elseif string.sub(code,i,i) == "<" and tpos > 0 then
       tpos = tpos - 1
     elseif string.sub(code,i,i) == "." then
+      io.write("\nInput: ")
       t[tpos] = tonumber(io.read())
     elseif string.sub(code,i,i) == "," then
+      io.write("\nOutput: ")
       io.write(t[tpos] .. "\n")
+    elseif string.sub(code,i,i) == "[" then
+      loopDepth = loopDepth + 1
+      loopTemp[loopDepth] = tpos
+      loopTempPos[loopDepth] = i
+    elseif string.sub(code,i,i) == "]" and t[loopTemp[loopDepth]] ~= 0 then
+      i = loopTempPos[loopDepth]
+    elseif string.sub(code,i,i) == "]" and t[loopTemp[loopDepth]] == 0 then
+      loopDepth = loopDepth - 1
     end
+    
+    if stepDisplay then
+      io.write("\n"..i.." "..loopDepth.."\n--------------------------------\n")
+      for j = 1, stepDisplaySize do
+        if j == tpos then
+          io.write("| ! ")
+        else
+          io.write("| "..j.." ")
+        end
+      end
+      io.write("\n--------------------------------\n")
+      for j = 1, stepDisplaySize do
+        io.write("| "..t[j].." ")
+      end
+      io.write("\n--------------------------------\n")
+    
+    end
+    
+    i = i+1
 end
 
 io.write("\n")
